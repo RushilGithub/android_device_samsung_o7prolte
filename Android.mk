b9001293cd097ebb016e7d278be6b7e117da51c6
Rushil <138013143+RushilGithub@users.noreply.github.com>
2023-07-17 11:27:40 +0530

    updatepart(1)

--- Android.mk
+++
@@ -1,7 +0,0 @@
-ifeq ($(TARGET_DEVICE),o7prolte)
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(CLEAR_VARS)
-
-endif
--- AndroidProducts.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := device/samsung/o7prolte
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_o7prolte.mk
-
-COMMON_LUNCH_CHOICES := \
-	lineage_o7prolte-eng \
-	lineage_o7prolte-userdebug \
-	lineage_o7prolte-user
