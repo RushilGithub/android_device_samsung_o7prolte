4a58bfc73dcb5b2a1d4646a50eb2abc0c96c6129
Rushil <138013143+RushilGithub@users.noreply.github.com>
2023-07-17 11:28:54 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := device/samsung/gprimeltexx
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/lineage_gprimeltexx.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_gprimeltexx-eng \
+	lineage_gprimeltexx-userdebug \
+	lineage_gprimeltexx-user
