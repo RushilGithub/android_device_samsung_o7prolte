80850c514e3fc79095039a435dc88fe09e1b865a
Rushil <138013143+RushilGithub@users.noreply.github.com>
2023-07-17 19:13:33 +0530

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -2,10 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=2
 
-# NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.nfc.fw_dl_on_boot=false
-
 # Screen
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=220
