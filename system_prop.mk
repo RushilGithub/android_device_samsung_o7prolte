ea9be42e4214124415317e900146487d1dc0c5aa
Rushil <138013143+RushilGithub@users.noreply.github.com>
2023-07-21 19:08:08 +0530

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -2,14 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0
 
-# NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.nfc.fw_dl_on_boot=false
-
 # EONS
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.eons.enabled=true
 
 # Screen
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=240
+	ro.sf.lcd_density=320
